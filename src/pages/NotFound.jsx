c2920ae1e9c66e9a621cb4ccdf0fec547797779c
Derek He <ilheduo@gmail.com>
2023-07-24 20:35:35 +1000

    add not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <div className="container">
+      <br /><br />
+      <div className="text-center">
+        <h1>Page not found</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
